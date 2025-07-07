02c93dac05e17ec8ea8c2f64649186ce517b545b
HarshithaGnanavel <harshithag.281003@gmail.com>
2025-07-07 10:26:00 +0530

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import ModeToggle from "@/components/ModeToggle";
+import {Button} from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  //server component by default
   return (
-    <div>
-      <h1>Hello Home page</h1>
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal" >
+          <Button>
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+    <ModeToggle />
+      <Button variant={"secondary"}>Click me</Button>
     </div>
   );
 }
